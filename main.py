4407a2f30bb75ee1ad114dcbc4ce5f2837a15bb8
Hari555Y <nitinishant8@gmail.com>
2022-08-29 21:56:38 +0530

    for plots

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def generate_random_points(D, n1, n2 , alpha):
                 points.append([x_cord, y_cord])
                 actual_points.append([x_cord, y_cord])
             if curgroup == 1:
-                for _ in range(int((alpha*n1)/100)):
+                for _ in range(int((n1*(alpha))/(100-alpha))):
                     x_cord = random.uniform(x_min, x_max)
                     y_cord = random.uniform(y_min, y_max)
                     actual_points.append([x_cord, y_cord])
@@ -92,51 +92,69 @@ def generate_k_clusters(k, points):
     return cluster, centroids
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     D = int(input("Enter the number of cuts in unit length: "))
     n1 = int(input("Enter the first density parameter: "))
     n2 = int(input("Enter the second density parameter: "))
   #  k = int(input("Enter the number of clusters: ")) 
    # k is now taken as a function of the number of points in the dataset
-    alpha = float(input("Enter the percentage of first density that is under-reported: "))
-    k1 = max(1 ,int((D*D*(n1+n2))/1000))
-    k2 = max(1, int((D*D*(n1  + int((alpha*n1)/100) + n2))/1000))
-    points , actual_points = generate_random_points(D, n1, n2 , alpha)
-   # actual_points = generate_random_points(D, n1  + int((alpha*n1)/100), n2)
-    cluster, centroids  = generate_k_clusters(k1, points)
-    actual_cluster, actual_centroids = generate_k_clusters(k2, actual_points)
-    sum_of_squares = 0
-    actual_sum_of_squares = 0
-    for point in points:
-        mini =1
-        for cen in centroids:
-            mini = min(mini, sq_distance(cen, point))
-        sum_of_squares += mini
-    for point in actual_points:
-        mini =1
-        for cen in actual_centroids:
-            mini = min(mini, sq_distance(cen, point))
-        actual_sum_of_squares += mini
-    cluster_colors = []
-    actual_cluster_colors = []
-    for _ in range(k1):
-        cluster_colors.append('#%06X' % random.randint(0, 0xFFFFFF))
-    for _ in range(k2):
-        actual_cluster_colors.append('#%06X' % random.randint(0, 0xFFFFFF))
-    print(centroids)
-    print(actual_centroids)
-    print(sum_of_squares)
-    print(actual_sum_of_squares)
-    figure, axis = plt.subplots(1,2)
+    # alpha = float(input("Enter the percentage of first density that is under-reported: "))
+    cost = []
+    for alpha in range(10,100,10):
+        k1 = max(1 ,int((D*D*(n1+n2))/50))
+        k2 = max(1, int((D*D*(n1  + int((alpha*n1)/(100-alpha)) + n2))/50))
+        points , actual_points = generate_random_points(D, n1, n2 , alpha)
+    # actual_points = generate_random_points(D, n1  + int((alpha*n1)/100), n2)
+        cluster, centroids  = generate_k_clusters(k1, points)
+        actual_cluster, actual_centroids = generate_k_clusters(k2, actual_points)
+        sum_of_squares = 0
+        actual_sum_of_squares = 0
+        for point in actual_points:
+            mini =1
+            for cen in centroids:
+                mini = min(mini, sq_distance(cen, point))
+            sum_of_squares += mini
+        # for point in actual_points:
+        #     mini =1
+        #     for cen in actual_centroids:
+        #         mini = min(mini, sq_distance(cen, point))
+        #     actual_sum_of_squares += mini
 
-    axis[0].scatter([point[0] for point in points], [point[1] for point in points], color=[
-                cluster_colors[cluster[i]] for i in range(len(points))])
-    axis[0].scatter([centroid[0] for centroid in centroids], [centroid[1]
-                                                          for centroid in centroids], color=['blue'])
-    axis[0].set_title("under-reported graph")
-    axis[1].scatter([point[0] for point in actual_points], [point[1] for point in actual_points], color=[
-                actual_cluster_colors[actual_cluster[i]] for i in range(len(actual_points))])
-    axis[1].scatter([centroid[0] for centroid in actual_centroids], [centroid[1]
-                                                          for centroid in actual_centroids], color=['blue'])    
-    axis[1].set_title("Actual graph")
+        # estimated_cluster = actual_cluster[:]
+        # for i in range(len(actual_cluster)):
+        #     point = actual_points[i]
+        #     mini = 1
+        #     cent = -1
+        #     for j in range(len(centroids)):
+        #         dista = sq_distance(centroids[j], point)
+        #         if dista < mini:
+        #             mini = dista
+        #             cent = j
+        #     estimated_cluster[i] = cent
+        # cluster_colors = []
+        # actual_cluster_colors = []
+        # for _ in range(k1):
+        #     cluster_colors.append('#%06X' % random.randint(0, 0xFFFFFF))
+        # for _ in range(k2):
+        #     actual_cluster_colors.append('#%06X' % random.randint(0, 0xFFFFFF))
+        # print(centroids)
+        # print(actual_centroids)
+        # print(sum_of_squares)
+        # print(actual_sum_of_squares)
+        cost.append(sum_of_squares)
+        
+        #figure, axis = plt.subplots(1,2)
+
+        # axis[0].scatter([point[0] for point in actual_points], [point[1] for point in actual_points], color=[
+        #             cluster_colors[estimated_cluster[i]] for i in range(len(actual_points))])
+        # axis[0].scatter([centroid[0] for centroid in centroids], [centroid[1]
+        #                                                     for centroid in centroids], color=['blue'])
+        # axis[0].set_title("under-reported graph")
+        # axis[1].scatter([point[0] for point in actual_points], [point[1] for point in actual_points], color=[
+        #             actual_cluster_colors[actual_cluster[i]] for i in range(len(actual_points))])
+        # axis[1].scatter([centroid[0] for centroid in actual_centroids], [centroid[1]
+        #                                                     for centroid in actual_centroids], color=['blue'])    
+        # axis[1].set_title("Actual graph")
+        # plt.show()
+    plt.plot(range(10,100,10), cost)
     plt.show()
